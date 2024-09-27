95f195936deccdcdfada6e591cedc87f5550b628
GolikovEA <ewgenei.golikow11@gmail.com>
2024-09-27 22:09:20 +0300

    lab1.2

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
     Numbers numbers = new Numbers();
     numbers.find();
+
+    Mid mid = new Mid();
+        String str1 = "string";
+        String str2 = "code";
+        String str3 = "Practice";
+
+        System.out.println("Средние символы для '" + str1 + "': " + mid.Midl(str1));
+        System.out.println("Средние символы для '" + str2 + "': " + mid.Midl(str2));
+        System.out.println("Средние символы для '" + str3 + "': " + mid.Midl(str3));
         }
     }
