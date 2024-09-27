a9f020023c2664d064a54e23159a70c02069d06d
GolikovEA <ewgenei.golikow11@gmail.com>
2024-09-27 22:37:58 +0300

    lab1.3

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -13,5 +13,10 @@ public class Main {
         System.out.println("Средние символы для '" + str1 + "': " + mid.Midl(str1));
         System.out.println("Средние символы для '" + str2 + "': " + mid.Midl(str2));
         System.out.println("Средние символы для '" + str3 + "': " + mid.Midl(str3));
+
+    MinDifferentSymbols min = new MinDifferentSymbols();
+        String text = "fffff ab f 1234 jkjk";
+        String minWord = min.find(text);
+        System.out.println("Слово с минимальным количеством различных символов: " + minWord);
         }
     }
