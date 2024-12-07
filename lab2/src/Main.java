142c87ce4f86ccb2b57ca399f1e9fd602a39b240
GolikovEA <ewgenei.golikow11@gmail.com>
2024-12-07 11:25:43 +0300

    lab2:mode

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Point point = new Point(3, 4);
         Circle circle = new Circle(point, 1);
-        circle.move(3, 5);
+        circle.move(3, 4);
         circle.setColor(Color.PURPLE);
         circle.draw();
 
