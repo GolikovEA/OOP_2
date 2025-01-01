f72b06fbc53adb011014378587a82568ea96eeff
GolikovEA <ewgenei.golikow11@gmail.com>
2025-01-01 15:21:29 +0300

    lab5

--- lab3/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Clothes[] clothes = {new Skirt(), new Tshirt(), new Trousers(), new Tie()};
-
-        Atelier atelier = new Atelier();
-        atelier.dressWomen(clothes);
-        atelier.dressMan(clothes);
-    }
-}
