8c70f256c8c6addd387c2c6ef74f313c5914f42b
GolikovEA <ewgenei.golikow11@gmail.com>
2025-01-01 15:54:12 +0300

    lab4

---
+++ lab4/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Reader reader = new Reader("C:\\Users\\user\\Desktop\\projekt\\OOP_2\\lab4\\file.txt");
+
+        for (int i = 0; i < reader.getSize(); i++){
+            String line = reader.getElement(i);
+            System.out.println(line);
+            String[] splitLine = line.split(":");
+            try {
+                System.out.println(Solver.findDecision(splitLine[1]));
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Хорошо:)");
+            }
+        }
+    }
+}
